75cc0f0588a138515782188ed2b3fc00306e1ffb
behlul.sahin <behlul.sahin@ANK5-YZLMORT-02>
2022-08-17 10:44:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//1234: Binler:1, Yüzler:2, Onlar:3, Birler:4
+
+string[] basamaklar = new string[]
+{
+    "Binler", "Yuzler", "Onlar", "Birler"
+};
+string ustSinir = "";
+foreach(string basamak in basamaklar)
+{
+    ustSinir += "9";
+}
+Console.Write($"{ustSinir} 'a kadar bir pozitif tam sayı giriniz: ");
+string giris = Console.ReadLine();
+string sonuc = "";
+
+for (int i = 0; i < giris.Length; i++)
+{
+    sonuc += basamaklar[i+basamaklar.Length-giris.Length] + " basamağı: " + giris[i] + "\n";
+    
+}
+Console.WriteLine(sonuc);
